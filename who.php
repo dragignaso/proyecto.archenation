774912b38afd5a7b3d72fe624f58265bf8954ca7
Cristian Tafolla Rosales <cristian.tafolla.rosales@gmail.com>
2019-05-26 22:11:52 -0500

    ADD: Estilos y descripción

---
+++ who.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Tienda virtual Archenation">
+    <meta name="author" content="Misael Camarillo, Marvin Rayas, Cristian Tafolla">
+
+  <title>Archenation - Quienes somos</title>
+
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <div class="container">
+          <a class="navbar-brand js-scroll-trigger" href="index.php">Archenation</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="who.php">Quienes somos
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="signinform.html">Registrese aqui</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="loginform.html">Iniciar sesión</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="contactoform.php">Contáctanos</a>
+                </li>
+            </ul>
+          </div>
+      </div>
+    </nav>
+
+  <header class="bg-primary py-5 mb-5">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+        <div class="col-lg-12">
+          <h1 class="display-4 text-white mt-5 mb-2">Archenation</h1>
+          <br>
+          <p class="lead mb-5 text-white-50">Somos tu primera opción si quieres adquirir equipo básico para la práctica de arquería o simplemente coleccionar armas de fantasía.</p>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+      <div class="col-md-8 mb-5">
+        <h2>Misión</h2>
+        <hr>
+        <p>Facilitar el acercamiento al deporte de arquería a las personas que están interesadas o están iniciándose mediante arcos de alta resistencia y calidad a un precio accesible con las características adecuadas para practicar dicho deporte, y al mismo tiempo elaborar arcos personalizados de bajo calibraje a todos aquellos interesados en coleccionar armas de fantasía a un precio económico.</p>
+      </div>
+      <div class="col-md-4 mb-5">
+        <h2>Nuestra ubicación</h2>
+        <hr>
+        <address>
+          <strong>Archenation Shop</strong>
+          <br>Andador Sigma 26
+          <br>San Rafael Chamapa, Naucalpan
+          <br>Estado de México
+          <br>
+        </address>
+        <address>
+          <abbr title="Nuestro teléfono">Teléfono:</abbr>
+          (56)1098-5755
+          <br>
+          <abbr title="Nuestro correo electrónico">Correo:</abbr>
+          <a href="mailto:archenation@gmail.com">archenation@gmail.com</a>
+        </address>
+      </div>
+      <div class="col-md-8 mb-5">
+        <h2>Visión</h2>
+        <hr>
+        <p>Ser la primera opción de aquellas personas que busquen adquirir equipo básico e intermedio para la práctica de arquería, así como de aquellas personas que coleccionan armas de fantasía en el territorio nacional.</p>
+      </div>
+      <div class="col-md-8 mb-5">
+        <h2>Nuestros valores</h2>
+        <hr>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/300x200" alt="inovacion">
+          <div class="card-body">
+            <h4 class="card-title">Competitividad e innovación</h4>
+            <p class="card-text">Cada uno de los productos que usted compre en Archenation lleva detrás un trabajo de diseño, preocupados por el funcionamiento óptimo.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/300x200" alt="respeto">
+          <div class="card-body">
+            <h4 class="card-title">Practicamos el trato respetuoso</h4>
+            <p class="card-text">Los acuerdos que haga con nosotros serán guiados por la motivación y aceptación, cumpliendo con la mejor disposición con sus pedidos.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/300x200" alt="integridad">
+          <div class="card-body">
+            <h4 class="card-title">Integridad laboral</h4>
+            <p class="card-text">Nos destacamos principalmente en actuar apegados a los principios anteriores, además de actuar siempre con honestidad.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <footer class="py-5 bg-dark">
+      <div class="container">
+          <p class="m-0 text-center text-white">Copyright &copy; Archenation. 2019</p>
+      </div>
+    
+    </footer>
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
