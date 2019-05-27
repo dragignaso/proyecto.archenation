f94a422ca0d2b4cb850985685d984cc4c0050bdc
Cristian Tafolla Rosales <cristian.tafolla.rosales@gmail.com>
2019-05-27 17:06:40 -0500

    ADD: Creditos y politicas

---
+++ credits.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Tienda virtual Archenation">
+    <meta name="author" content="Misael Camarillo, Marvin Rayas, Cristian Tafolla">
+
+  <title>Archenation - Nuestras Políticas</title>
+
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <div class="container">
+          <a class="navbar-brand js-scroll-trigger" href="index.php">Archenation</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="who.php">Quienes somos</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="signinform.html">Registrese aqui</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="loginform.html">Iniciar sesión</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="contactoform.php">Contáctanos</a>
+                </li>
+            </ul>
+          </div>
+      </div>
+    </nav>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h1>Créditos</h1>
+          <hr>
+          <br>
+          <h2>Dirección general</h2>
+          <p class="lead">Marvin Rayas Sánchez</p>
+          <br>
+          <hr>
+          <h2>Operaciones web</h2>
+          <p class="lead">Misael Camarillo Rangel</p>
+          <br>
+          <hr>
+          <h2>Mercadotecnia</h2>
+          <p class="lead">Cristian Tafolla Rosales</p>
+          <br>
+          <hr>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<footer class="py-5 bg-dark">
+      <div class="container">
+          <p class="m-0 text-center text-white">Copyright &copy; Archenation. 2019 <br><br> <a href="credits.php">Créditos</a> | <a href="politics.php">Políticas</a> </p>
+          <br>
+          
+      </div>
+    
+    </footer>
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
---
+++ politics.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Tienda virtual Archenation">
+    <meta name="author" content="Misael Camarillo, Marvin Rayas, Cristian Tafolla">
+
+  <title>Archenation - Nuestras Políticas</title>
+
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <div class="container">
+          <a class="navbar-brand js-scroll-trigger" href="index.php">Archenation</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="who.php">Quienes somos</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="signinform.html">Registrese aqui</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="loginform.html">Iniciar sesión</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="contactoform.php">Contáctanos</a>
+                </li>
+            </ul>
+          </div>
+      </div>
+    </nav>
+
+  <section id="compra">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Políticas de compra</h2>
+          <ul>
+            <li>El cliente puede navegar sin ninguna complicación en el sitio en línea, sin embargo, para realizar una compra dentro de nuestro sitio se tiene que contar con una cuenta en el sistema, de lo contrario no se podrán realizar ninguna venta.</li>
+            <li>Los datos solicitados son únicamente para la correcta entrega del producto al domicilio del cliente.</li>
+            <li>En caso de que el producto ordenado, fuera a dejar de estar disponible por falta en stock, Archenation no dejará agregar dicho producto al carrito para evitar conflictos a posterior.</li>
+            <li>Los precios de los productos incluidos en esta página, están en pesos mexicanos, todos los precios incluyen IVA y están sujetos a cambio sin previo aviso.</li>
+            <li>Al adquirir cualquiera de nuestros productos, el cliente asume la responsabilidad de que el producto es el indicado para su manejo y Archenation no se hace responsable del uso indebido del uso de nuestros productos.</li>
+            <li>Después de haber completado la compra y realizado el pago, no será posible cancelar un pedido.</li>
+            <li>Cualquier duda, inconformidad o incidente, que pudiera derivarse de una transacción realizada en <a href="index.php">http://www.archenation.com</a>, que no esté comprendido en estas políticas de compra, haga favor de notificarnoslo mediante nuestra forma de contacto en línea o de comunicarnosla al número (55)53028624 o al teléfono celular (55)66672273, juntos analizaremos su caso para encontrar la solución más justa para ambas partes.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="pago">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Políticas de pago</h2>
+          <ul>
+            <li>Para realizar transacciones en este sitio, aceptamos tarjetas de crédito tipo Visa &copy; y MasterCard &copy;. Si no cuenta con una tarjeta de crédito de estos tipos y desea comprar en nuestra página, favor de comunicarse al 53028624 o al teléfono celular (55) 66672273.</li>
+            <li>El precio que se muestra al efectuar la transacción incluye costo del envío, y del producto, además del IVA asociado a este.</li>
+            <li>El costo del envío será calculado en función del destino y el número de piezas incluidas en el pedido. (Se incluye en el total de la compra).</li>
+            <li>La introducción de datos falsos o incorrectos para la entrega del pedido queda bajo responsabilidad del cliente y en caso de generarse, éste deberá asumir los costos adicionales de envío derivados del incidente, o el pedido quedará cancelado sin responsabilidad para Archenation &copy; ni devolución del dinero.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+    <section id="envio">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Políticas de envío</h2>
+          <ul>
+            <li>Por el momento, sólo atendemos pedidos dentro de la República Mexicana. La entrega de productos sólo se realiza mediante envío terrestre.</li>
+            <li>Archenation &copy; realiza sus envíos mediante la compañía Estafeta &copy; Mexicana S.A de C.V. que le permite a los clientes, rastrear su pedido en su sitio Web http://www.estafeta.com.mx, mediante un código de rastreo que se le asigna al momento de procesar su compra en línea en <a href="index.php">http://www.archenation.com</a></li>
+            <li>La política de Archenation &copy; es entregar su pedido en un plazo no mayor a 7 días, el plazo de entrega comienza a contar a partir de la confirmación de su pago por el banco.</li>
+            <li>Águila Azteca &copy; no asume responsabilidad en caso de demoras en la entrega, imputables a Estafeta Mexicana S.A de C.V. aunque hace hincapié en que esta empresa de paquetería cuenta con garantía de entrega para todos los envíos.</li>
+            <li>Archenation &copy; se reserva el derecho de utilizar otros medios para entregar sus pedidos en caso de alguna contingencia.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="devolucion">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Políticas de devolución</h2>
+          <ul>
+            <li>En caso de extravío durante el traslado, Archenation &copy; se compromete a reponer al cliente, los productos incluidos en el pedido.</li>
+            <li>Nuestros productos cuentan con un tres meses de garantía por defecto de fabricación; en estos casos es posible hacer la devolución del producto que será reemplazado por uno igual o equivalente, siempre y cuando el cliente se comprometa a pagar los gastos del envío del producto dañado desde su domicilio a nuestras instalaciones físicas, así como los del envío del nuevo producto al domicilio indicado por el cliente.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="py-5 bg-dark">
+      <div class="container">
+          <p class="m-0 text-center text-white">Copyright &copy; Archenation. 2019 <br><br> <a href="credits.php">Créditos</a> | <a href="politics.php">Políticas</a> </p>
+          <br>
+          
+      </div>
+    
+    </footer>
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
