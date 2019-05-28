ba3701c0c3d32e5e38c6f14d7c0b69b753a2dab9
dragignaso <misacamarillo141@gmail.com>
2019-05-28 07:19:16 -0500

    Carrito 80%

--- modCarrito.php
+++ modCarrito.php
@@ -7,8 +7,26 @@
 		$idVenta = $_POST['idventa'];
 
 		$querycliente = "DELETE FROM contenidoVenta WHERE idVenta = ".$idVenta."AND idArticulo = ".$idArticulo;
+		$queryconscontventa = "SELECT cantidad,precioFinal FROM contenidoVenta WHERE idVenta = ".$idVenta."AND idArticulo = ".$idArticulo;
         $conn = pg_connect("host=127.0.0.1 port=5432 dbname=archenationbd user=archenationuser password=archeuser1234") or die();
+        $result = pg_query($conn, $queryconscontventa) or die (pg_last_error());
+        $row = pg_fetch_row($result);
+        $cantidad = $row[0];
+        $precioFinal = $row[1];
+        $descuento = $cantidad * $precioFinal;
+
+        $queryconstotventa = "SELECT totalVenta FROM venta WHERE idVenta = ".$idVenta;
+        $result = pg_query($conn, $queryconscontventa) or die (pg_last_error());
+        $row = pg_fetch_row($result);
+        $totalActual = $row[0];
+
+        $descuento = $totalActual - $descuento;
+
+        $querymodventa = "UPDATE venta SET totalVenta = ".$descuento." WHERE idVenta = ".$idVenta;
+        $result = pg_query($conn, $querymodventa) or die (pg_last_error());
+
         $result = pg_query($conn, $querycliente) or die (pg_last_error());
+
         header("Location: carrito.php");
 	}
 
