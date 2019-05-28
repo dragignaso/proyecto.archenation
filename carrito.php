fe35b50ebae20a5bee5eaf28d0203c75426047e6
Cristian Tafolla Rosales <cristian.tafolla.rosales@gmail.com>
2019-05-28 00:16:19 -0500

    ADD: Layout Carrito
    Ah spmd estoy mamadisimo

---
+++ carrito.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Tienda virtual Archenation">
+    <meta name="author" content="Misael Camarillo, Marvin Rayas, Cristian Tafolla">
+
+  <title>Archenation - Carrito</title>
+
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="css/style.css" rel="stylesheet">
+<link rel="icon" type="image/vnd.microsoft.icon" href="img/logo.ico" sizes="16x16 24x24 36x36 48x48">
+
+<body>
+	 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <div class="container">
+          <a class="navbar-brand js-scroll-trigger" href="index.php">Archenation</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#">Tu carrito</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#">Cerrar sesión</a>
+                </li>
+            </ul>
+          </div>
+      </div>
+    </nav>
+
+<div class="card mb-3">
+          <div class="card-header">
+            <i class="fas fa-table"></i>
+            Data Table Example</div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <h1>Tu carrito de compras</h1>
+              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                  <tr>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Precio</th>
+                    <th>Cantidad</th>
+                    <th>Total</th>
+                    <th>IVA</th>
+                  </tr>
+                </thead>
+                <tfoot>
+                  <tr>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Precio</th>
+                    <th>Cantidad</th>
+                    <th>Total</th>
+                    <th>IVA</th>
+                  </tr>
+                </tfoot>
+                <tbody>
+                  <tr>
+                    <td>Arco Trifuerza</td>
+                    <td>Está bien bonito</td>
+                    <td>$700.00</td>
+                    <td>1</td>
+                    <td>$700.00</td>
+                    <td>$112.00</td>
+                  </tr>
+                  <tr>
+                    <td>Arco Trifuerza</td>
+                    <td>Está bien bonito</td>
+                    <td>$700.00</td>
+                    <td>1</td>
+                    <td>$700.00</td>
+                    <td>$112.00</td>
+                  </tr>
+                  
+
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <!-- /.container-fluid -->
+
+
+<footer class="py-5 bg-dark">
+      <div class="container">
+          <p class="m-0 text-center text-white">Copyright &copy; Archenation. 2019 <br><br> <a href="credits.php">Créditos</a> | <a href="politics.php">Políticas</a> </p>
+          <br>
+          
+      </div>
+    
+    </footer>
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
