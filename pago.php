e473bc3296b47fb0235a718fc477f98729a9784d
Cristian Tafolla Rosales <cristian.tafolla.rosales@gmail.com>
2019-05-27 23:52:09 -0500

    ADD: Confirmación pago

---
+++ pago.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Tienda virtual Archenation">
+    <meta name="author" content="Misael Camarillo, Marvin Rayas, Cristian Tafolla">
+
+  <title>Archenation - Confirmación</title>
+
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="css/style.css" rel="stylesheet">
+<link rel="icon" type="image/vnd.microsoft.icon" href="img/logo.ico" sizes="16x16 24x24 36x36 48x48">
+
+<body>
+	 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <div class="container">
+          <a class="navbar-brand js-scroll-trigger" href="index.php">Archenation</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#">Tu carrito</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#">Cerrar sesión</a>
+                </li>
+            </ul>
+          </div>
+      </div>
+    </nav>
+
+  	<header class="bg-primary text-white">
+    	<div class="container text-center">
+    		<h2>Resumen de la compra</h2>
+        <hr>
+    		<h4>Referencia:</h4>
+        <p>XXXXXXXXXXXXX</p>
+        <h4>Descripción:</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce viverra turpis in quam sagittis venenatis. Cras non elit sit amet nulla tristique finibus. Etiam lacinia orci nec libero interdum, eget vehicula justo fermentum</p>
+        <h4>Total a pagar:</h4>
+        <p>$0000.00</p>
+    		<hr>
+    	</div>
+  	</header>
+
+<footer class="py-5 bg-dark">
+      <div class="container">
+          <p class="m-0 text-center text-white">Copyright &copy; Archenation. 2019 <br><br> <a href="credits.php">Créditos</a> | <a href="politics.php">Políticas</a> </p>
+          <br>
+          
+      </div>
+    
+    </footer>
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
